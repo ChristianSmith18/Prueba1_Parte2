5bc967e456e9223526f13f663d55c167d58e23aa
Christian Smith <cgr.gonzalezrossier@gmail.com>
2020-06-09 18:51:41 -0400

    Cración de los métodos Insertar

--- Prueba1.java
+++ Prueba1.java
@@ -24,4 +24,27 @@ class Lista {
   public static void main(String[] args) {
 
   }
+
+  public void insertar(int x, int p) {
+    NodoLista aux = new NodoLista(x);
+    NodoLista temp = cabeza;
+    int i = 0;
+    if (p < 0)
+      System.out.println("Posicion inválida!");
+    else {
+      while ((temp.siguiente != null) && (i != p)) {
+        i++;
+        temp = temp.siguiente;
+      }
+      if (i < p)
+        System.out.println("La posición indicada no existe!");
+      else
+        insertar(aux, temp);
+    }
+  }
+
+  private void insertar(NodoLista x, NodoLista p) {
+    x.siguiente = p.siguiente;
+    p.siguiente = x;
+  }
 }
